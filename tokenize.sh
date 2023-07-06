aaf6d0ddff01304139741ae4e3b0ea8de1959c7e
Chen Liu <59649479+FrankMinions@users.noreply.github.com>
2023-07-06 17:24:34 +0800

    Update tokenize.sh

--- tokenize.sh
+++ tokenize.sh
@@ -1,8 +1,9 @@
 CUDA_VISIBLE_DEVICES=0,1 python tokenize_dataset_rows.py \
     --model_checkpoint THUDM/chatglm2-6b \
     --input_file sentiment_comp_ie.json \
-    --prompt_key q \
-    --target_key a \
+    --instruct_key instruction \
+    --prompt_key input \
+    --target_key output \
     --save_name sentiment_comp_ie_chatglm2 \
-    --max_seq_length 2000 \
+    --max_seq_length 1024 \
     --skip_overlength False
